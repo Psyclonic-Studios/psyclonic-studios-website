8f2d4c4fa8dfcec3d31d0bcca5a83eb583656d87
hwinning <hwinning1@gmail.com>
2019-11-20 14:11:14 +1100

    fix firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as request from 'request';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,14 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.refreshContributeProducts = functions.firestore
+.document('fl_content/{documentId}')
+.onWrite((change, context) => {
+    const document = change.after.exists ? change.after.data() : null;
+    if(document && document._fl_meta_.schema === 'supportProducts') {
+        request.get('https://psyclonicstudios.com.au/refresh_contribute_products')
+        return 0;
+    } else {
+        return 1;
+    }
+})
