01b80042f18f31eadc4681d553029116bae124f4
hwinning <hwinning1@gmail.com>
2019-11-25 12:21:33 +1100

    seo stuff

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask_sitemap import Sitemap
 from datetime import datetime
 from babel import numbers
 from server import crud
+from slugify import slugify
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+sitemap = Sitemap(app=app)
 
 @app.route('/')
 def home():
     return render_template('home.html', home_images=crud.get_home_images(crud.TRANSACTION, 999)['image_urls'], home_text=crud.get_home_text())
 
+@sitemap.register_generator
+def sitemap_home():
+    yield 'home', {}
+
 @app.route('/about')
 def about():
     return render_template('about.html', about=crud.get_about())
+
+@sitemap.register_generator
+def sitemap_about():
+    yield 'about', {}
 
 @app.route('/contribute')
 def contribute():
@@ -19,6 +33,10 @@ def contribute():
     number_of_products = len(contribute_products)
     donation_skus = crud.get_donation_skus()
     return render_template('contribute.html', support_text=crud.get_contribute_text(), contribute_products=contribute_products, number_of_products=number_of_products, donation_skus=donation_skus, shipping_sku=crud.get_shipping_sku())
+
+@sitemap.register_generator
+def sitemap_contribute():
+    yield 'contribute', {}
 
 @app.route('/refresh_contribute_products')
 def refresh_contribute_products():
@@ -30,22 +48,34 @@ def artwork_collection():
     artworks = crud.get_artwork_collection(crud.TRANSACTION, 375, args=request.args)
     return render_template('artwork_collection.html', artworks=artworks)
 
-@app.route('/artwork/<string:id>')
-def artwork(id):
+@sitemap.register_generator
+def sitemap_artwork_collection():
+    yield 'artwork_collection', {}
+
+@app.route('/artwork/<string:id>', defaults={'slug': None})
+@app.route('/artwork/<slug>/<string:id>')
+def artwork(slug, id):
     artwork = crud.get_artwork(crud.TRANSACTION, id, 667)
     number_of_images = len(artwork['image_urls'])
-    return render_template('artwork.html', artwork=artwork, number_of_tiles=number_of_images)
+    canonical_url = url_for('artwork', slug=slugify_title(artwork["title"]), id=id, _external=True)
+    return render_template('artwork.html', artwork=artwork, number_of_tiles=number_of_images, canonical_url=canonical_url)
 
 @app.route('/series', strict_slashes=False)
 def series_collection():
     series_collection = crud.get_series_collection(crud.TRANSACTION, 375, args=request.args)
     return render_template('series_collection.html', series_collection=series_collection)
 
-@app.route('/series/<string:id>')
-def series(id):
+@sitemap.register_generator
+def sitemap_series_collection():
+    yield 'series_collection', {}
+
+@app.route('/series/<string:id>', defaults={'slug': None})
+@app.route('/series/<slug>/<string:id>')
+def series(slug, id):
     series = crud.get_series(crud.TRANSACTION, id, 667)
     number_of_tiles = len(series['artworks'])
-    return render_template('series.html', series=series, number_of_tiles=number_of_tiles)
+    canonical_url = url_for('series', slug=slugify_title(series["title"]), id=id, _external=True)
+    return render_template('series.html', series=series, number_of_tiles=number_of_tiles, canonical_url=canonical_url)
 
 #@app.route('/blog', strict_slashes=False)
 #def blog_collection():
@@ -91,6 +121,10 @@ def format_date(datetime_str):
 @app.template_filter('format_money')
 def format_money(money_str):
     return numbers.format_currency(money_str, 'AUD', locale='en_AU')
+
+@app.template_filter('slugify')
+def slugify_title(txt):
+    return slugify(txt, max_length=25)
 
 @app.context_processor
 def inject_year():
